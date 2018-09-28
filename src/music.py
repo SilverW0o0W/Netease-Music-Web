fe3bc583980f4f981336eb313ce193346976b974
SilverW0o0W <silver.codingcat@gmail.com>
2018-09-28 20:19:05 +0800

    Update element

---
+++ src/music.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3.6
+# -*- encoding: utf-8 -*-
+
+
+import os
+import sys
+import json
+
+import toml
+
+import tornado
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+import tornado.template
+from tornado.log import access_log
+
+
+class WebBase(tornado.web.RequestHandler):
+    def prepare(self):
+        if self.request.body:
+            try:
+                self.params = json.loads(self.request.body)
+            except:
+                self.params = {}
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("%s port /path/to/config_file" % (sys.argv[0]))
+        exit(1)
+    if not os.path.isfile(sys.argv[2]):
+        print("config file %s not exist" % (sys.argv[2]))
+        exit(1)
+    with open(sys.argv[2], "r") as f:
+        Config = toml.load(f)
+
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/views/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(os.path.dirname(__file__), "views")}),
+        ],
+        debug=True,
+    )
+
+    http_server = tornado.httpserver.HTTPServer(app, xheaders=True)
+    http_server.listen(int(sys.argv[1]))
+    tornado.ioloop.IOLoop.current().start()
